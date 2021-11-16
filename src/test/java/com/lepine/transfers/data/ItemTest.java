9bfd005a859bf6b1a53aef611b6abc6cab7b0f43
Fube <fubeitch@gmail.com>
2021-11-15 23:21:15 -0500

    Added context loads test

--- src/test/java/com/lepine/transfers/data/ItemTest.java
+++ src/test/java/com/lepine/transfers/data/ItemTest.java
@@ -1,6 +1,9 @@
 package com.lepine.transfers.data;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -10,7 +13,15 @@ import static org.junit.jupiter.api.Assertions.*;
 @ActiveProfiles({"test"})
 class ItemTest {
 
+    @Autowired
+    ItemRepo itemRepo;
+
     @BeforeEach
     void setUp() {
+        itemRepo.deleteAll();
     }
+
+    @Test
+    @DisplayName("Context loads")
+    void contextLoads() {}
 }
