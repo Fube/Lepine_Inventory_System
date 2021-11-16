50e4f7ae7c1eb2fd5236edc5c0d2f485df4cb706
Fube <fubeitch@gmail.com>
2021-11-15 23:13:19 -0500

    Added Item entity

---
+++ src/main/java/com/lepine/transfers/data/Item.java
@@ -0,0 +1,28 @@
+package com.lepine.transfers.data;
+
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Table(schema = "lepine")
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Item {
+
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Id
+    private UUID uuid = UUID.randomUUID();
+
+    private String SKU;
+    private String description;
+    private String name;
+}
