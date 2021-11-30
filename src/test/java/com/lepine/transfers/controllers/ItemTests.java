4c6f1b6c73d1ac2293916b662fe32dfa8452ab7b
Fube <fubeitch@gmail.com>
2021-11-29 23:04:38 -0500

    Added controllers/ItemTests

---
+++ src/test/java/com/lepine/transfers/controllers/ItemTests.java
@@ -0,0 +1,19 @@
+package com.lepine.transfers.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles({"test"})
+public class ItemTests {
+
+    void setUp() {
+
+    }
+
+    @Test
+    void contextLoads() {
+
+    }
+}
