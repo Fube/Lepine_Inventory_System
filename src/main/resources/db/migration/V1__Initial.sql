8666a1fd6e7a03c5dd155e5c1f026e04aaa7b183
Fube <fubeitch@gmail.com>
2021-12-02 23:06:50 -0500

    Updated V1 migration to match domain

--- src/main/resources/db/migration/V1__Initial.sql
+++ src/main/resources/db/migration/V1__Initial.sql
@@ -1,15 +1,8 @@
 CREATE SCHEMA IF NOT EXISTS lepine;
 
-/**
-      private UUID uuid = UUID.randomUUID();
-
-    private String SKU;
-    private String description;
-    private String name;
- */
 CREATE TABLE lepine.items(
     uuid VARCHAR(36) PRIMARY KEY,
-    sku VARCHAR(8),
-    name VARCHAR(255),
+    sku VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
     description VARCHAR(1023)
 )
