ce55bcc8d5ef160ea4e26eeaed00fa1558cfd38c
Fube <fubeitch@gmail.com>
2021-12-02 19:22:52 -0500

    Added failing ItemServiceTests::saveItem

---
+++ src/main/java/com/lepine/transfers/services/search/SearchService.java
@@ -0,0 +1,5 @@
+package com.lepine.transfers.services.search;
+
+public interface SearchService<T> {
+    void index(T toIndex);
+}
--- src/test/java/com/lepine/transfers/services/ItemServiceTests.java
+++ src/test/java/com/lepine/transfers/services/ItemServiceTests.java
@@ -3,21 +3,26 @@ package com.lepine.transfers.services;
 import com.lepine.transfers.data.item.Item;
 import com.lepine.transfers.data.item.ItemRepo;
 import com.lepine.transfers.services.item.ItemService;
+import com.lepine.transfers.services.search.SearchService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Comparator;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @ActiveProfiles({"test"})
@@ -28,6 +33,15 @@ public class ItemServiceTests {
 
     @Autowired
     private ItemService itemService;
+
+    @MockBean
+    private SearchService<Item> searchService;
+
+    private static Comparator<Item> itemComparator;
+
+    static {
+        itemComparator = Comparator.comparing(Item::getSKU);
+    }
 
     @Test
     void contextLoads(){}
@@ -94,5 +108,27 @@ public class ItemServiceTests {
             assertTrue(item.getDescription().startsWith("description"));
         });
         verify(itemRepo, times(1)).findAll(pageRequest);
+    }
+
+    @Test
+    @DisplayName("Given valid item, create item and send copy to SearchService")
+    void saveItem() {
+
+        // Arrange
+        final Item item = Item.builder()
+                .name("name")
+                .SKU("SKU")
+                .description("description")
+                .build();
+        doNothing().when(searchService).index(any(Item.class));
+
+        // Act
+        final Item saved = itemService.create(item);
+
+        // Assert
+        assertEquals(item, saved);
+        verify(itemRepo, times(1)).save(item);
+        verify(searchService, times(1)).index(
+                argThat(i -> itemComparator.compare(item, (Item)i) == 0));
     }
 }
