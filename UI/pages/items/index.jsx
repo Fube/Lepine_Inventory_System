2e8a9f2ddae9f00e6bf5815ea47c68bb022be5eb
Fube <fubeitch@gmail.com>
2021-12-08 21:56:02 -0500

    We do a little refactoring

--- UI/pages/items/index.jsx
+++ UI/pages/items/index.jsx
@@ -15,43 +15,52 @@ export default function ShowItems({ items, totalPages, pageNumber }) {
     const loadNewPage = (newPage) => {
         router.push(`/items?page=${newPage}`);
     };
+
+    const getTableOrSadFace = () => {
+        if (items && items.length <= 0) {
+            return (
+                <h2 className="text-2xl text-center text-yellow-400">
+                    Nothing to show 😢
+                </h2>
+            );
+        }
+
+        const mappedItems = items.map(({ uuid, name, sku, description }) => (
+            <Link key={uuid} href={`/items/${uuid}`} passHref>
+                <tr className="hover">
+                    <td>{name}</td>
+                    <td>{sku}</td>
+                    <td>{description}</td>
+                </tr>
+            </Link>
+        ));
+        return (
+            <table className="table table-zebra w-full table-fixed">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>SKU</th>
+                        <th>Description</th>
+                    </tr>
+                </thead>
+                <tbody>{mappedItems}</tbody>
+            </table>
+        );
+    };
+
     return (
         <>
             <Nav />
-            <h1 className="text-4xl text-center my-4">Items</h1>
             <div className="overflow-x-auto justify-center flex">
                 <div className="w-1/2">
-                    <table className="table table-zebra w-full">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>SKU</th>
-                                <th>Description</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {items.map(({ uuid, name, sku, description }) => (
-                                <Link
-                                    key={uuid}
-                                    href={`/items/${uuid}`}
-                                    passHref
-                                >
-                                    <tr className="hover">
-                                        <td>{name}</td>
-                                        <td>{sku}</td>
-                                        <td>{description}</td>
-                                    </tr>
-                                </Link>
-                            ))}
-                        </tbody>
-                    </table>
-
+                    <h1 className="text-4xl text-left my-4">Items</h1>
+                    {getTableOrSadFace()}
                     {totalPages > 1 && (
                         <div className="flex justify-center mt-4">
                             <Paginate
                                 pageNumber={pageNumber}
                                 totalPages={totalPages}
-                                onPageChange={(page) => loadNewPage(page)}
+                                onPageChange={loadNewPage}
                             />
                         </div>
                     )}
