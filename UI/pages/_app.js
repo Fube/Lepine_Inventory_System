f4eafe787670c477c7bfbd95a08d2afc213a941c
Fube <fubeitch@gmail.com>
2021-12-08 22:12:59 -0500

    Added algolia stuff

--- UI/pages/_app.js
+++ UI/pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { InstantSearch } from "react-instantsearch-dom";
+import algoliasearch from "algoliasearch/lite";
+
+const searchClient = algoliasearch(
+    "latency",
+    "6be0576ff61c053d5f9a3225e2a90f76"
+);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <InstantSearch searchClient={searchClient} indexName="instant_search">
+            <Component {...pageProps} />
+        </InstantSearch>
+    );
 }
 
-export default MyApp
+export default MyApp;
