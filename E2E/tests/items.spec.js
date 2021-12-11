84d4abc9bc8d237fa10953f7d98d2f48e4714052
Fube <fubeitch@gmail.com>
2021-12-11 00:45:16 -0500

    Added goto /items/new E2E test

--- E2E/tests/items.spec.js
+++ E2E/tests/items.spec.js
@@ -27,3 +27,33 @@ test("Go to /items through nav", async ({ page }) => {
     expect(await active.textContent()).toBe("Items");
     expect(await active.getAttribute("href")).toBe("/items");
 });
+
+test("Go to /items/new through /items", async ({ page }) => {
+    await page.goto("/items");
+
+    // Get the "Add One Now!" button
+    const addOneNow = await page.$("//html/body/div[1]/main/div/button");
+    expect(addOneNow).toBeTruthy();
+
+    // Click on "Add One Now!" button
+    await Promise.all([
+        page.waitForNavigation({ waitUntil: "networkidle0" }),
+        await addOneNow.click(),
+    ]);
+
+    // Check that the page is loaded
+    const title = await page.title();
+    expect(title).toBe("Add Item");
+
+    // Check form is present
+    const form = await page.$("form");
+    expect(form).toBeTruthy();
+
+    // Check form contains input fields
+    const inputFields = await page.$("form input");
+    expect(inputFields.length).toBe(3);
+    const inputFieldsNames = await Promise.all(
+        inputFields.map((n) => n.getProperty("name"))
+    );
+    expect(inputFieldsNames).toEqual(["name", "description", "sku"]);
+});
