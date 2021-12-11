abe03972cd859df88c25fd4b07d91a2f33406b2b
Fube <fubeitch@gmail.com>
2021-12-11 00:07:56 -0500

    Added E2E setup

---
+++ E2E/playwright.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    globalSetup: require.resolve("./tests/global-setup.js"),
+    use: {
+        baseURL: process.env.BASE_URL ?? "http://localhost:3000",
+        headless: true,
+        viewport: { width: 1280, height: 720 },
+        ignoreHTTPSErrors: true,
+        screenshot: "only-on-failure",
+        timeout: 30000,
+    },
+};
