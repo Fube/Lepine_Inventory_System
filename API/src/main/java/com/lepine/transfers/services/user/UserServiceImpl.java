0798cef2208e919a7e281d346fefd39da36bb0e1
Fube <fubeitch@gmail.com>
2021-12-13 13:59:03 -0500

    Added UserServiceImpl skeleton

---
+++ API/src/main/java/com/lepine/transfers/services/user/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.lepine.transfers.services.user;
+
+import com.lepine.transfers.data.user.User;
+import com.lepine.transfers.data.user.UserUUIDLessDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public User create(UserUUIDLessDTO userUUIDLessDTO) {
+        return null;
+    }
+}
