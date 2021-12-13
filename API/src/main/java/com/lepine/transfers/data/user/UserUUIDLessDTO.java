44878a0b42d8989067f4eda5068f4b8d92210736
Fube <fubeitch@gmail.com>
2021-12-13 12:47:31 -0500

    Added UserUUIDLessDTO

---
+++ API/src/main/java/com/lepine/transfers/data/user/UserUUIDLessDTO.java
@@ -0,0 +1,20 @@
+package com.lepine.transfers.data.user;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class UserUUIDLessDTO {
+    @Email(message = "Email must be a valid email address")
+    private String email;
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "Password must be at least 8 characters long, include a number, inclue a capital letter, inclue a special character")
+    private String password;  
+}
