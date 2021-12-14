2831e0c17f4dfd426e493af460e3181968e2ac44
Fube <fubeitch@gmail.com>
2021-12-13 20:36:18 -0500

    Added UserService::findAll spec

--- API/src/main/java/com/lepine/transfers/services/user/UserService.java
+++ API/src/main/java/com/lepine/transfers/services/user/UserService.java
@@ -3,8 +3,11 @@ package com.lepine.transfers.services.user;
 import com.lepine.transfers.data.user.User;
 import com.lepine.transfers.data.user.UserUUIDLessDTO;
 
+import java.util.List;
+
 public interface UserService {
 
     User create(UserUUIDLessDTO userUUIDLessDTO);
-    
+
+    List<User> findAll();
 }
