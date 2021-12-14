dff427cae81e6a678b6b13377cc14777cc06ed99
Fube <fubeitch@gmail.com>
2021-12-13 20:30:01 -0500

    Added failing UserController::getAll tests

--- API/src/test/java/com/lepine/transfers/controllers/UserControllerTests.java
+++ API/src/test/java/com/lepine/transfers/controllers/UserControllerTests.java
@@ -13,14 +13,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import static com.lepine.transfers.helpers.PageHelpers.createPageFor;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
@@ -33,6 +38,17 @@ public class UserControllerTests {
     private static final String INVALID_EMAIL = "oogabooga";
     private static final String VALID_PASSWORD = "S0m3P@ssw0rd";
     private static final String INVALID_PASSWORD = "invalidpassword";
+
+    private static List<User> seedUsers(int num) {
+        return IntStream.range(0, num)
+                .mapToObj(i -> User.builder()
+                        .uuid(UUID.randomUUID())
+                        .email(i+VALID_EMAIL)
+                        .password(VALID_PASSWORD)
+                        .build()
+                )
+                .collect(Collectors.toList());
+    }
 
     @Autowired
     private UserController userController;
@@ -310,5 +326,121 @@ public class UserControllerTests {
                         "Password must not be blank")));
 
         verify(userService, times(0)).create(userUUIDLessDTO);
+    }
+
+    @Test
+    @DisplayName("Given no page number or size, then retrieve first page of Users")
+    void retrieveAllUsers_noPageRequest() {
+
+        // Arrange
+        final int
+                total = 100,
+                page = 0,
+                size = 10;
+        final Page<User> pageFor = createPageFor(seedUsers(total));
+        given(userService.getAll(page, size)).willReturn(pageFor);
+
+        // Act
+        Page<UserUUIDLessDTO> got = userController.getAll(pageable);
+
+        // Assert
+        assertEquals(0, got.getNumber());
+        assertEquals(10, got.getSize());
+        assertEquals(1, got.getTotalPages());
+        assertEquals(1, got.getTotalElements());
+        assertEquals(0, got.getNumberOfElements());
+        assertEquals(0, got.getContent().size());
+    }
+
+    @Test
+    @DisplayName("Given negative page number and valid size, then throw ConstraintViolationException")
+    void retrieveAllUsers_negativePageNumber() {
+
+        // Arrange
+        final int
+                total = 100,
+                page = -1,
+                size = 10;
+        final Page<User> pageFor = createPageFor(seedUsers(total));
+        given(userService.getAll(page, size)).willReturn(pageFor);
+
+        // Act
+        ConstraintViolationException exception =
+                assertThrows(ConstraintViolationException.class, () -> userController.getAll(pageable));
+
+        // Assert
+        final Set<ConstraintViolation<?>> constraintViolations = exception.getConstraintViolations();
+        assertFalse(constraintViolations.isEmpty());
+        assertEquals(1, constraintViolations.size());
+
+        final Set<String> collect = constraintViolations.stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.toSet());
+        assertTrue(
+                collect.contains("Page number must be greater than or equal to 0"));
+
+        verify(userService, times(0)).getAll(page, size);
+    }
+
+    @Test
+    @DisplayName("Given valid page but negative size, then throw ConstraintViolationException")
+    void retrieveAllUsers_negativeLimit() {
+
+        // Arrange
+        final int
+                total = 100,
+                page = 0,
+                size = -1;
+        final Page<User> pageFor = createPageFor(seedUsers(total));
+        given(userService.getAll(page, size)).willReturn(pageFor);
+
+        // Act
+        ConstraintViolationException exception =
+                assertThrows(ConstraintViolationException.class, () -> userController.getAll(pageable));
+
+        // Assert
+        final Set<ConstraintViolation<?>> constraintViolations = exception.getConstraintViolations();
+        assertFalse(constraintViolations.isEmpty());
+        assertEquals(1, constraintViolations.size());
+
+        final Set<String> collect = constraintViolations.stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.toSet());
+        assertTrue(
+                collect.contains("Page size must be greater than or equal to 1"));
+
+        verify(userService, times(0)).getAll(page, size);
+    }
+
+    @Test
+    @DisplayName("Given negative page number and negative size, then throw ConstraintViolationException")
+    void retrieveAllUsers_negativePageNumberAndNegativeSize() {
+
+        // Arrange
+        final int
+                total = 100,
+                page = -1,
+                size = -1;
+        final Page<User> pageFor = createPageFor(seedUsers(total));
+        given(userService.getAll(page, size)).willReturn(pageFor);
+
+        // Act
+        ConstraintViolationException exception =
+                assertThrows(ConstraintViolationException.class, () -> userController.getAll(pageable));
+
+        // Assert
+        final Set<ConstraintViolation<?>> constraintViolations = exception.getConstraintViolations();
+        assertFalse(constraintViolations.isEmpty());
+        assertEquals(2, constraintViolations.size());
+
+        final Set<String> collect = constraintViolations.stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.toSet());
+        assertTrue(
+                collect.containsAll(List.of(
+                        "Page number must be greater than or equal to 0",
+                        "Page size must be greater than or equal to 1")));
+
+        verify(userService, times(0)).getAll(page, size);
     }
 }
