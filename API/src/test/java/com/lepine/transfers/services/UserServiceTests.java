f1d288a63f2ec850898cec76080b8d882f13f2b9
Fube <fubeitch@gmail.com>
2021-12-14 11:27:11 -0500

    Added failing UserService::findAll test

--- API/src/test/java/com/lepine/transfers/services/UserServiceTests.java
+++ API/src/test/java/com/lepine/transfers/services/UserServiceTests.java
@@ -11,10 +11,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import static com.lepine.transfers.helpers.PageHelpers.createPageFor;
 import static java.lang.String.format;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -29,6 +34,17 @@ public class UserServiceTests {
 
     private final static String VALID_EMAIL = "some@email.com";
     private final static String VALID_PASSWORD = "S0m3P@ssw0rd";
+
+    private static List<User> generateUsers(int num) {
+        List<User> items = new ArrayList<>();
+        for (int i = 0; i < num; i++) {
+            items.add(User.builder()
+                    .email(i+VALID_EMAIL)
+                    .password(VALID_PASSWORD)
+                    .build());
+        }
+        return items;
+    }
 
     @Autowired
     private UserService userService;
@@ -90,5 +106,22 @@ public class UserServiceTests {
 
         verify(userRepo, times(1)).findByEmail(VALID_EMAIL);
         verify(userRepo, times(0)).save(any());
+    }
+
+    @Test
+    @DisplayName("Given no PageRequest, then return first page of Users")
+    void findAll() {
+
+        // Arrange
+        final int num = 100;
+        final Page<User> pageFor = createPageFor(generateUsers(num), PageRequest.of(0, 10));
+        given(userRepo.findAll(any(PageRequest.class))).willReturn(pageFor);
+
+        // Act
+        final Page<User> all = userService.findAll();
+
+        // Assert
+        assertEquals(pageFor, all);
+        verify(userRepo, times(1)).findAll(any(PageRequest.class));
     }
 }
