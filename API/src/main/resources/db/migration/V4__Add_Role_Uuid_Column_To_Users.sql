fdcc5cc5cb64b9e868b569071038001b1c12c4fe
Fube <fubeitch@gmail.com>
2021-12-15 18:06:53 -0500

    Fixed Role accepting null references

--- API/src/main/resources/db/migration/V4__Add_Role_Uuid_Column_To_Users.sql
+++ API/src/main/resources/db/migration/V4__Add_Role_Uuid_Column_To_Users.sql
@@ -1,3 +1,6 @@
 ALTER TABLE lepine.users
-    ADD COLUMN role_uuid uuid
-        REFERENCES lepine.roles(uuid) ON DELETE CASCADE;
+    ADD COLUMN role_uuid uuid NOT NULL;
+
+ALTER TABLE lepine.users
+    ADD CONSTRAINT fk_customer
+        FOREIGN KEY(role_uuid) REFERENCES lepine.roles(uuid);
