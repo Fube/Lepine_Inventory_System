7dd936c06e638ad126e3b9aba0960d851d331a9c
Fube <fubeitch@gmail.com>
2021-12-15 14:52:52 -0500

    Added AuthControllerTests skeleton

---
+++ API/src/test/java/com/lepine/transfers/controllers/AuthControllerTests.java
@@ -0,0 +1,14 @@
+package com.lepine.transfers.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(classes = {  })
+@ActiveProfiles({"test"})
+public class AuthControllerTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
