9b69b6cf582fbd003e2ffd4d185b1b0db1b5eaed
Fube <fubeitch@gmail.com>
2021-12-15 18:46:10 -0500

    Got AuthControllerTests passing

--- API/src/main/java/com/lepine/transfers/controllers/auth/AuthController.java
+++ API/src/main/java/com/lepine/transfers/controllers/auth/AuthController.java
@@ -4,6 +4,8 @@ import com.lepine.transfers.data.auth.UserLogin;
 import com.lepine.transfers.data.user.User;
 import com.lepine.transfers.data.user.UserMapper;
 import com.lepine.transfers.data.user.UserPasswordLessDTO;
+import com.lepine.transfers.exceptions.auth.InvalidLoginException;
+import com.lepine.transfers.exceptions.user.UserNotFoundException;
 import com.lepine.transfers.services.auth.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,7 +25,15 @@ public class AuthController {
 
     public UserPasswordLessDTO login(@Valid UserLogin userLogin) {
         log.info("Logging in user {}", userLogin.getEmail());
-        final User login = authService.login(userLogin);
+
+        User login;
+        try {
+            login = authService.login(userLogin);
+        } catch (UserNotFoundException e) {
+            log.error("User with email {} not found", userLogin.getEmail());
+            throw new InvalidLoginException();
+        }
+
         log.info("User {} logged in", login.getUsername());
 
         return userMapper.toPasswordLessDTO(login);
