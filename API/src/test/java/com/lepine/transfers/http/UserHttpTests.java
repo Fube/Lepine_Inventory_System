cd6ff9c681f771703c6f8a464e1e1a23bb2c2197
Fube <fubeitch@gmail.com>
2021-12-14 22:04:59 -0500

    Added UserHttpTests::create_AsManager_NullEmail

--- API/src/test/java/com/lepine/transfers/http/UserHttpTests.java
+++ API/src/test/java/com/lepine/transfers/http/UserHttpTests.java
@@ -226,6 +226,35 @@ public class UserHttpTests {
     }
 
     @Test
+    @DisplayName("Given POST on /users with valid password but null email as manager , then return 400")
+    @WithMockUser(username = "some-manager", roles = "MANAGER")
+    void create_AsManager_NullEmail() throws Exception {
+
+        // Arrange
+        final UserUUIDLessDTO userUUIDLessDTO = UserUUIDLessDTO.builder()
+                .email(null)
+                .password(VALID_PASSWORD)
+                .build();
+        // Act
+        final ResultActions resultActions = mvc.perform(
+                post("/users")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userUUIDLessDTO)));
+
+        // Assert
+        resultActions.andExpect(status().isBadRequest())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").value("Invalid request"))
+                .andExpect(jsonPath("$.status").value(400))
+                .andExpect(jsonPath("$.timestamp").exists())
+                .andExpect(jsonPath("$.errors").exists())
+                .andExpect(jsonPath("$.errors.email.length()").value(1))
+                .andExpect(jsonPath("$.errors.email[0]")
+                        .value(messageSource.getMessage("user.email.not_blank", null, Locale.getDefault())))
+                .andExpect(jsonPath("$.errors.password").doesNotExist());
+    }
+
+    @Test
     @DisplayName("Given POST on /users with valid data as clerk, then return 403")
     @WithMockUser(username = "some-user", roles = "CLERK")
     void create_AsClerk() throws Exception {
