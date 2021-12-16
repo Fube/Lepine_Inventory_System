568669c10e6a2081e2e3d5b565026777449fe12e
Fube <fubeitch@gmail.com>
2021-12-15 21:38:41 -0500

    Got AuthServiceTests passing

--- API/src/main/java/com/lepine/transfers/services/auth/AuthService.java
+++ API/src/main/java/com/lepine/transfers/services/auth/AuthService.java
@@ -3,6 +3,8 @@ package com.lepine.transfers.services.auth;
 import com.lepine.transfers.data.auth.UserLogin;
 import com.lepine.transfers.data.user.User;
 
+import javax.validation.Valid;
+
 public interface AuthService {
-    User login(UserLogin userLogin);
+    User login(@Valid UserLogin userLogin);
 }
