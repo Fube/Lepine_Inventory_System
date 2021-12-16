00258672a5a39c661cc1cbed070aa05fc4bfb59f
Fube <fubeitch@gmail.com>
2021-12-16 10:40:30 -0500

    Updated AuthService to match last change

--- API/src/main/java/com/lepine/transfers/services/auth/AuthService.java
+++ API/src/main/java/com/lepine/transfers/services/auth/AuthService.java
@@ -2,9 +2,10 @@ package com.lepine.transfers.services.auth;
 
 import com.lepine.transfers.data.auth.UserLogin;
 import com.lepine.transfers.data.user.User;
+import org.javatuples.Pair;
 
 import javax.validation.Valid;
 
 public interface AuthService {
-    User login(@Valid UserLogin userLogin);
+    Pair<User, String> login(@Valid UserLogin userLogin);
 }
