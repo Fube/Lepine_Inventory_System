125cb5ca189bbb77f09afedc4f1f5146c70204df
Fube <fubeitch@gmail.com>
2021-12-16 11:50:29 -0500

    Added SecurityConfig::configure(AuthenticationManagerBuilder impl

--- API/src/main/java/com/lepine/transfers/config/SecurityConfig.java
+++ API/src/main/java/com/lepine/transfers/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.lepine.transfers.config;
 
+import com.lepine.transfers.data.user.UserRepo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.List;
@@ -15,7 +18,13 @@ import java.util.Map;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final UserRepo userRepo;
+
     private static final Map<HttpMethod, List<String>> whiteListByMethod = Map.of();
+
+    public SecurityConfig(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -36,4 +45,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         expressionInterceptUrlRegistry
                 .anyRequest().authenticated();
     }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        final DaoAuthenticationConfigurer dao = new DaoAuthenticationConfigurer<>(username ->
+                userRepo.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email " + username + "not found")));
+
+        dao.passwordEncoder(passwordEncoder());
+
+        auth.apply(dao);
+    }
+
 }
