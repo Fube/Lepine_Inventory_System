3eb0f7edd205a7d06cc0950c40a8a5e39e519204
Fube <fubeitch@gmail.com>
2021-12-17 00:08:10 -0500

    Added RoleRepo::findByName spec

--- API/src/main/java/com/lepine/transfers/data/role/RoleRepo.java
+++ API/src/main/java/com/lepine/transfers/data/role/RoleRepo.java
@@ -4,8 +4,10 @@ import com.lepine.transfers.data.auth.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface RoleRepo extends JpaRepository<Role, UUID> {
+    Optional<Role> findByName(String name);
 }
