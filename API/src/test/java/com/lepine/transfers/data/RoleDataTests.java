b0503d7e48c052b69f07db9aa91831b59bc6abc9
Fube <fubeitch@gmail.com>
2021-12-17 00:09:17 -0500

    Added RoleDataTests::retrieveByName

--- API/src/test/java/com/lepine/transfers/data/RoleDataTests.java
+++ API/src/test/java/com/lepine/transfers/data/RoleDataTests.java
@@ -10,9 +10,11 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
 @ActiveProfiles({"test"})
@@ -37,5 +39,21 @@ public class RoleDataTests {
         assertEquals(3, all.size());
         assertThat(all.stream().map(Role::getName))
                 .containsExactlyInAnyOrder("MANAGER", "SALESPERSON", "CLERK");
+    }
+
+    @Test
+    @DisplayName("Given existing role, retrieve by name")
+    void retrieveByName(){
+        // Arrange
+        final String name = "MANAGER";
+
+        // Act
+        final Optional<Role> role = roleRepo.findByName(name);
+
+        // Assert
+        assertTrue(role.isPresent());
+
+        final Role retrieved = role.get();
+        assertEquals(name, retrieved.getName());
     }
 }
