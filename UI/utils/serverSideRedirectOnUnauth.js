81b62cd886a5e750a7663d72e454205683096679
Fube <fubeitch@gmail.com>
2021-12-17 10:56:11 -0500

    Added disclamier to serverSideRedirectOnUnauth

--- UI/utils/serverSideRedirectOnUnauth.js
+++ UI/utils/serverSideRedirectOnUnauth.js
@@ -1,3 +1,9 @@
+/**
+ * NOTE:
+ *  This god forsaken pattern I have come up with is really bad
+ *  but I cannot find a better solution with getInitalProps gone.
+ *  Perhaps a HOC would be better. This is to be revised before more pages are added.
+ */
 export default async function serverSideRedirectOnUnauth(
     act,
     destination = "/login"
