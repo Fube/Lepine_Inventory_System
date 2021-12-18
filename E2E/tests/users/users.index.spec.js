6cd293c6bf47b926735372741e0e734fa7428398
Fube <fubeitch@gmail.com>
2021-12-18 15:50:11 -0500

    Added /users spec

---
+++ E2E/tests/users/users.index.spec.js
@@ -0,0 +1,37 @@
+const { test, expect } = require("@playwright/test");
+
+test("/users :: Go to from /index", async ({ page, browser }) => {
+    // Go to home
+    await Promise.all([
+        page.waitForNavigation({ waitUntil: "networkidle0" }),
+        page.goto("/"),
+    ]);
+
+    // Click on "Users" button
+    await Promise.all([
+        page.waitForNavigation({ waitUntil: "networkidle0" }),
+        page.click("[href*=users]"),
+    ]);
+
+    // Check that the page is loaded
+    const title = await page.title();
+    expect(title).toBe("Users");
+
+    // Check that the page contains a table or a message saying that there are no users
+    const table = await page.$("table");
+    if (table) {
+        // Check that the table contains a header
+        const header = await page.$eval("table thead tr", (n) => n.innerText);
+        expect(header).toBeTruthy();
+        expect(header.toLowerCase()).toContain("email");
+        expect(header.toLowerCase()).toContain("role");
+
+        // Check that the table contains a row
+        const rows = await page.$$("table tbody tr");
+        expect(rows.length).toBeGreaterThan(0);
+    } else {
+        const message = await page.$eval("p", (n) => n.innerText);
+        expect(message).toBeTruthy();
+        expect(message.toLowerCase()).toContain("no users");
+    }
+});
