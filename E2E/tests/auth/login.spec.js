18654168b1fed3693a62d8a133f528f9283f8986
Fube <fubeitch@gmail.com>
2021-12-18 15:18:52 -0500

    Added login test

--- E2E/tests/auth/login.spec.js
+++ E2E/tests/auth/login.spec.js
@@ -34,3 +34,39 @@ test("/login :: Go to through /index", async ({ page, browser }) => {
     expect(inputFieldsNames).toContain("email");
     expect(inputFieldsNames).toContain("password");
 });
+
+test("/login :: Login with valid information", async ({ page }) => {
+    // Goto login
+    await Promise.all([
+        page.waitForNavigation({ waitUntil: "networkidle0" }),
+        page.click("[href*=login]"),
+    ]);
+
+    // Fill in form
+    await page.type('input[name="email"]', process.env.USERNAME ?? "manager");
+    await page.type(
+        'input[name="password"]',
+        process.env.PASSWORD ?? "manager"
+    );
+
+    // Submit form
+    await Promise.all([
+        page.waitForNavigation({ waitUntil: "networkidle0" }),
+        page.click("button[type=submit]"),
+    ]);
+
+    // Check that the page is loaded
+    const title = await page.title();
+    expect(title).toBe("Home");
+
+    // Check that the user is logged in
+    const localStorage = await page.evaluate(() => {
+        return localStorage;
+    });
+
+    expect(localStorage.email).toBeTruthy();
+    expect(localStorage.role).toBeTruthy();
+
+    const cookies = await page.context().cookies();
+    expect(cookies.find((n) => n.name === "token")).toBeTruthy();
+});
