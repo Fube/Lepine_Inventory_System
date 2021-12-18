846ee3a338b1e4d22e7fd6829c1bf3350b6ef71c
Fube <fubeitch@gmail.com>
2021-12-18 14:23:05 -0500

    Added login to global-setup

--- E2E/tests/global-setup.js
+++ E2E/tests/global-setup.js
@@ -1,2 +1,29 @@
-// Login logic will be added here later
-module.exports = () => {};
+const { chromium } = require("@playwright/test");
+module.exports = async (config) => {
+    const { baseURL, storageState } = config.projects[0].use;
+    const browser = await chromium.launch({
+        headless: false,
+    });
+    const page = await browser.newPage();
+    await page.goto(baseURL);
+
+    await Promise.all([
+        page.waitForNavigation({ waitUntil: "networkidle0" }),
+        page.click("[href*=login]"),
+    ]);
+
+    await page.type('input[name="email"]', process.env.USERNAME ?? "manager");
+    await page.type(
+        'input[name="password"]',
+        process.env.PASSWORD ?? "manager"
+    );
+
+    await Promise.all([
+        page.waitForNavigation({ waitUntil: "networkidle0" }),
+        page.click("button[type=submit]"),
+    ]);
+
+    await page.context().storageState({ path: storageState });
+
+    await browser.close();
+};
