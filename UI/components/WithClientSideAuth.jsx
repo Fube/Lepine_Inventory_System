4bb7951e77a24e4a4e97f84f0a9b12ae827ac156
Fube <fubeitch@gmail.com>
2021-12-23 21:43:38 -0500

    Show loading text to prevent page flashing

--- UI/components/WithClientSideAuth.jsx
+++ UI/components/WithClientSideAuth.jsx
@@ -1,17 +1,33 @@
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import roleRouteMappings from "../config/routeRoleMapping";
+import useAuth from "../hooks/useAuth";
+import clientRedirectHelper from "../utils/clientRedirectHelper";
+
 export default function WithClientSideAuth(WrappedComponent) {
     return function (props) {
         const router = useRouter();
         const { role } = useAuth();
+        const [loading, setLoading] = useState(true);
 
-        useEffect(
+        useEffect(() => {
             clientRedirectHelper(
                 roleRouteMappings.get(router.asPath),
                 role.toLocaleLowerCase(),
                 router
-            ),
-            []
-        );
+            )();
+            setLoading(false);
+        }, []);
 
+        if (loading) {
+            return (
+                <div className="flex justify-center h-screen">
+                    <div className="flex flex-col justify-center">
+                        <h1>Loading...</h1>
+                    </div>
+                </div>
+            );
+        }
         return <WrappedComponent {...props} />;
     };
 }
