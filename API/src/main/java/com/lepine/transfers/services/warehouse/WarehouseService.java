56f34950089dad5f03815ce8858de3c5d996843a
Fube <fubeitch@gmail.com>
2022-01-03 18:17:00 -0500

    Added WarehouseService

---
+++ API/src/main/java/com/lepine/transfers/services/warehouse/WarehouseService.java
@@ -0,0 +1,7 @@
+package com.lepine.transfers.services.warehouse;
+
+import com.lepine.transfers.data.item.Item;
+
+public interface WarehouseService {
+    Item create(Item item);
+}
--- API/src/test/java/com/lepine/transfers/services/WarehouseServiceTests.java
+++ API/src/test/java/com/lepine/transfers/services/WarehouseServiceTests.java
@@ -1,12 +1,22 @@
 package com.lepine.transfers.services;
 
+import com.lepine.transfers.data.warehouse.WarehouseRepo;
+import com.lepine.transfers.services.warehouse.WarehouseService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = {})
 @ActiveProfiles({"test"})
 public class WarehouseServiceTests {
+
+    @Autowired
+    private WarehouseService warehouseService;
+
+    @MockBean
+    private WarehouseRepo warehouseRepo;
 
     @Test
     void contextLoads(){}
