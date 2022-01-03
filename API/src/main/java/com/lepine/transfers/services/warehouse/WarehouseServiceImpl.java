dfc2dad329583b2f04358a136f3941c8ce05b062
Fube <fubeitch@gmail.com>
2022-01-03 18:18:31 -0500

    Added WarehouseServiceImpl

---
+++ API/src/main/java/com/lepine/transfers/services/warehouse/WarehouseServiceImpl.java
@@ -0,0 +1,10 @@
+package com.lepine.transfers.services.warehouse;
+
+import com.lepine.transfers.data.item.Item;
+
+public class WarehouseServiceImpl implements WarehouseService {
+    @Override
+    public Item create(Item item) {
+        return null;
+    }
+}
--- API/src/test/java/com/lepine/transfers/services/WarehouseServiceTests.java
+++ API/src/test/java/com/lepine/transfers/services/WarehouseServiceTests.java
@@ -2,13 +2,14 @@ package com.lepine.transfers.services;
 
 import com.lepine.transfers.data.warehouse.WarehouseRepo;
 import com.lepine.transfers.services.warehouse.WarehouseService;
+import com.lepine.transfers.services.warehouse.WarehouseServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = {})
+@SpringBootTest(classes = {WarehouseServiceImpl.class})
 @ActiveProfiles({"test"})
 public class WarehouseServiceTests {
 
