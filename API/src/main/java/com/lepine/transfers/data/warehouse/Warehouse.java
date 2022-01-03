c00fd08a68bcd38677a846d8d293ea0e992f1d5a
Fube <fubeitch@gmail.com>
2022-01-03 17:08:32 -0500

    Added Warehouse entity

---
+++ API/src/main/java/com/lepine/transfers/data/warehouse/Warehouse.java
@@ -0,0 +1,32 @@
+package com.lepine.transfers.data.warehouse;
+
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Table(schema = "lepine", name = "warehouses")
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Warehouse {
+
+    @Builder.Default
+    @GeneratedValue(generator = "UUIDGenerator")
+    @GenericGenerator(name = "UUIDGenerator", strategy = "uuid2")
+    @Id
+    private UUID uuid = UUID.randomUUID();
+
+    private String zipCode;
+    private String city;
+    private String province;
+    private boolean isActive;
+}
