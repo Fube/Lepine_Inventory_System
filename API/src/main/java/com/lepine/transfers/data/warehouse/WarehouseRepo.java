216ea99012ed973d0f70887b840a5ef11aa18bab
Fube <fubeitch@gmail.com>
2022-01-03 17:07:22 -0500

    Added WarehouseRepo

---
+++ API/src/main/java/com/lepine/transfers/data/warehouse/WarehouseRepo.java
@@ -0,0 +1,9 @@
+package com.lepine.transfers.data.warehouse;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface WarehouseRepo extends JpaRepository<Warehouse, UUID> {
+}
