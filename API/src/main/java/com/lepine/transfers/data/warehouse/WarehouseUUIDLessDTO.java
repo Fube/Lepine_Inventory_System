dc186bdb1381a7fd6ea5bf1a4a7b17234c71641b
Fube <fubeitch@gmail.com>
2022-01-04 17:44:13 -0500

    Got pQDYgoMlxF & EurotOZVRn passing

--- API/src/main/java/com/lepine/transfers/data/warehouse/WarehouseUUIDLessDTO.java
+++ API/src/main/java/com/lepine/transfers/data/warehouse/WarehouseUUIDLessDTO.java
@@ -17,10 +17,14 @@ public class WarehouseUUIDLessDTO {
     @NotNull(message = "{warehouse.zipcode.not_null}")
     @NotBlank(message = "{warehouse.zipcode.not_blank}")
     private String zipCode;
+
+    @NotNull(message = "{warehouse.city.not_null}")
+    @NotBlank(message = "{warehouse.city.not_blank}")
     private String city;
 
     @NotNull(message = "{warehouse.province.not_null}")
     @NotBlank(message = "{warehouse.province.not_blank}")
     private String province;
+
     private boolean active;
 }
