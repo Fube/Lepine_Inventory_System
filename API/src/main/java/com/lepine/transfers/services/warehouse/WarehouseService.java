f910f639457718ea71bcd2d32cd70f6c7170534d
Fube <fubeitch@gmail.com>
2022-01-04 18:12:01 -0500

    Added WarehouseService::findAll spec

--- API/src/main/java/com/lepine/transfers/services/warehouse/WarehouseService.java
+++ API/src/main/java/com/lepine/transfers/services/warehouse/WarehouseService.java
@@ -3,6 +3,8 @@ package com.lepine.transfers.services.warehouse;
 import com.lepine.transfers.data.warehouse.Warehouse;
 import com.lepine.transfers.data.warehouse.WarehouseActiveLessUUIDLessDTO;
 import com.lepine.transfers.data.warehouse.WarehouseUUIDLessDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 import javax.validation.Valid;
 import java.util.UUID;
@@ -13,4 +15,8 @@ public interface WarehouseService {
     void delete(UUID uuid);
 
     Warehouse update(UUID uuid, @Valid WarehouseUUIDLessDTO toUpdate);
+
+    Page<Warehouse> findAll();
+
+    Page<Warehouse> findAll(PageRequest pageRequest);
 }
