0ac98565550f3c914ef5aca8643d3b3c087f5f78
Fube <fubeitch@gmail.com>
2022-01-03 21:19:45 -0500

    Added WarehouseMapper

---
+++ API/src/main/java/com/lepine/transfers/data/warehouse/WarehouseMapper.java
@@ -0,0 +1,14 @@
+package com.lepine.transfers.data.warehouse;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface WarehouseMapper {
+
+    @Mappings({
+            @Mapping(target = "uuid", ignore = true),
+    })
+    Warehouse toEntity(WarehouseActiveLessUUIDLessDTO warehouseActiveLessUUIDLessDTO);
+}
--- API/src/main/java/com/lepine/transfers/services/warehouse/WarehouseServiceImpl.java
+++ API/src/main/java/com/lepine/transfers/services/warehouse/WarehouseServiceImpl.java
@@ -2,6 +2,7 @@ package com.lepine.transfers.services.warehouse;
 
 import com.lepine.transfers.data.warehouse.Warehouse;
 import com.lepine.transfers.data.warehouse.WarehouseActiveLessUUIDLessDTO;
+import com.lepine.transfers.data.warehouse.WarehouseMapper;
 import com.lepine.transfers.data.warehouse.WarehouseRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 public class WarehouseServiceImpl implements WarehouseService {
 
     private final WarehouseRepo warehouseRepo;
+    private final WarehouseMapper warehouseMapper;
 
     @Override
     public Warehouse create(WarehouseActiveLessUUIDLessDTO warehouse) {
