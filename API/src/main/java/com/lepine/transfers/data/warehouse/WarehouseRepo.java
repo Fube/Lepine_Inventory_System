f923e69446a4260a089af6b551c84a80f3aa9354
Fube <fubeitch@gmail.com>
2022-01-03 23:16:24 -0500

    Added WarehouseRepo::findByZipCode spec

--- API/src/main/java/com/lepine/transfers/data/warehouse/WarehouseRepo.java
+++ API/src/main/java/com/lepine/transfers/data/warehouse/WarehouseRepo.java
@@ -3,8 +3,11 @@ package com.lepine.transfers.data.warehouse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 @Repository
 public interface WarehouseRepo extends JpaRepository<Warehouse, UUID> {
     Integer deleteByUuid(UUID uuid);
+
+    Optional<Warehouse> findByZipCode(String zipCode);
 }
