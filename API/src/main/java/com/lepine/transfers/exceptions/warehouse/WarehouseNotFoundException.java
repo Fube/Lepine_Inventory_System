c0a5303ef46ac5caad094495121d913e8af8657d
Fube <fubeitch@gmail.com>
2022-01-04 16:58:44 -0500

    Got VnvttdRxZP passing

--- API/src/main/java/com/lepine/transfers/exceptions/warehouse/WarehouseNotFoundException.java
+++ API/src/main/java/com/lepine/transfers/exceptions/warehouse/WarehouseNotFoundException.java
@@ -2,10 +2,12 @@ package com.lepine.transfers.exceptions.warehouse;
 
 import com.lepine.transfers.exceptions.NotFoundException;
 
+import java.util.UUID;
+
 import static java.lang.String.format;
 
 public class WarehouseNotFoundException extends NotFoundException {
-    public WarehouseNotFoundException(String message) {
-        super(format("Warehouse with uuid %s not found", message));
+    public WarehouseNotFoundException(UUID uuid) {
+        super(format("Warehouse with uuid %s not found", uuid));
     }
 }
