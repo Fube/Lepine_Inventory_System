7bede3c250c9559ca61a52ca45263f1817da50c5
Fube <fubeitch@gmail.com>
2022-01-04 18:32:15 -0500

    Got hKqgNkfEDG passing

--- API/src/main/java/com/lepine/transfers/controllers/warehouse/WarehouseController.java
+++ API/src/main/java/com/lepine/transfers/controllers/warehouse/WarehouseController.java
@@ -5,16 +5,20 @@ import com.lepine.transfers.data.warehouse.WarehouseActiveLessUUIDLessDTO;
 import com.lepine.transfers.services.warehouse.WarehouseService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@Validated
 public class WarehouseController {
 
     private final WarehouseService warehouseService;
 
-    public Warehouse create(WarehouseActiveLessUUIDLessDTO warehouseActiveLessUUIDLessDTO) {
+    public Warehouse create(@Valid WarehouseActiveLessUUIDLessDTO warehouseActiveLessUUIDLessDTO) {
         log.info("Attempting to create a new warehouse {}", warehouseActiveLessUUIDLessDTO);
         final Warehouse warehouse = warehouseService.create(warehouseActiveLessUUIDLessDTO);
         log.info("Warehouse created {}", warehouse);
