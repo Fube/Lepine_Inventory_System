d297b0d30f9395121c5cae900bef18a46e18eb77
Fube <fubeitch@gmail.com>
2022-01-05 03:24:14 -0500

    Added FjWippwTSO

--- API/src/test/java/com/lepine/transfers/http/WarehouseHttpTests.java
+++ API/src/test/java/com/lepine/transfers/http/WarehouseHttpTests.java
@@ -1,27 +1,92 @@
 package com.lepine.transfers.http;
 
+import com.algolia.search.com.fasterxml.jackson.databind.ObjectMapper;
 import com.lepine.transfers.config.AuthConfig;
+import com.lepine.transfers.config.MapperConfig;
 import com.lepine.transfers.config.ValidationConfig;
 import com.lepine.transfers.controllers.warehouse.WarehouseController;
+import com.lepine.transfers.data.warehouse.Warehouse;
+import com.lepine.transfers.data.warehouse.WarehouseActiveLessUUIDLessDTO;
+import com.lepine.transfers.data.warehouse.WarehouseMapper;
 import com.lepine.transfers.services.warehouse.WarehouseService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.UUID;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.atMostOnce;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = { WarehouseController.class })
-@ContextConfiguration(classes = { ValidationConfig.class, AuthConfig.class })
+@ContextConfiguration(classes = { MapperConfig.class, ValidationConfig.class, AuthConfig.class })
 @ActiveProfiles("test")
 public class WarehouseHttpTests {
+
+    private final static String
+            VALID_CITY = "City",
+            VALID_ZIP = "A1B2C3",
+            VALID_PROVINCE = "Province",
+            ERROR_FORMAT_MESSAGE_DUPLICATE_ZIP = "Zipcode %s already in use",
+            ERROR_FORMAT_MESSAGE_WAREHOUSE_NOT_FOUND = "Warehouse with uuid %s not found";
+    private final static UUID
+            VALID_UUID = UUID.randomUUID();
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Autowired
     private WarehouseController warehouseController;
 
+    @Autowired
+    private WarehouseMapper warehouseMapper;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockBean
     private WarehouseService warehouseService;
 
+
     @Test
     void contextLoads() {}
+
+    @Test
+    @DisplayName("FjWippwTSO: Given POST on /warehouses with valid warehouse dto as manager, then return created (201, warehouse)")
+    void create_AsManager() throws Exception {
+
+        // Arrange
+        final WarehouseActiveLessUUIDLessDTO given = WarehouseActiveLessUUIDLessDTO.builder()
+                .city(VALID_CITY)
+                .zipCode(VALID_ZIP)
+                .province(VALID_PROVINCE)
+                .build();
+        final String asString = objectMapper.writeValueAsString(given);
+
+        final Warehouse expected = warehouseMapper.toEntity(given);
+
+        given(warehouseService.create(given))
+                .willReturn(expected);
+
+        // Act
+        final ResultActions perform = mockMvc.perform(post("/warehouses")
+                .contentType("application/json")
+                .content(asString));
+
+        // Assert
+        perform.andExpect(status().isCreated())
+                .andExpect(content().json(objectMapper.writeValueAsString(expected)));
+
+        verify(warehouseService, atMostOnce()).create(given);
+    }
 }
