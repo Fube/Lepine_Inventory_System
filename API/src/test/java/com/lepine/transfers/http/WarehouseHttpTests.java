7d0474dce24ae42379ca96ecce61b04788fd9d32
Fube <fubeitch@gmail.com>
2022-01-05 16:53:29 -0500

    Added GQIstxKYso

--- API/src/test/java/com/lepine/transfers/http/WarehouseHttpTests.java
+++ API/src/test/java/com/lepine/transfers/http/WarehouseHttpTests.java
@@ -838,4 +838,20 @@ public class WarehouseHttpTests {
 
         verify(warehouseService, never()).update(VALID_UUID, given);
     }
+
+    @Test
+    @DisplayName("GQIstxKYso: Given PUT on /warehouses/{uuid} with null city as clerk, then return forbidden (403, error)")
+    @WithMockUser(username = "some-clerk", roles = "CLERK")
+    void update_AsClerk_WithNullCity() throws Exception {
+
+        // Arrange
+        final WarehouseUUIDLessDTO given = VALID_WAREHOUSE_UUID_LESS_DTO;
+        given.setCity(null);
+
+        // Act & Assert
+        updateWith(VALID_UUID, given, null)
+                .andExpect(status().isForbidden());
+
+        verify(warehouseService, never()).update(VALID_UUID, given);
+    }
 }
