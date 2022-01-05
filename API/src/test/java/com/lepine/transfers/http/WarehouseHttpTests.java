7a897cdbf44b117faccadfa10a65bafb9574daa3
Fube <fubeitch@gmail.com>
2022-01-05 03:26:41 -0500

    Run with mock user

--- API/src/test/java/com/lepine/transfers/http/WarehouseHttpTests.java
+++ API/src/test/java/com/lepine/transfers/http/WarehouseHttpTests.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -63,6 +64,7 @@ public class WarehouseHttpTests {
 
     @Test
     @DisplayName("FjWippwTSO: Given POST on /warehouses with valid warehouse dto as manager, then return created (201, warehouse)")
+    @WithMockUser(username = "some-manager", roles = "MANAGER")
     void create_AsManager() throws Exception {
 
         // Arrange
