6230dc873239e0a034b3c934ee8c99ad674ab847
Fube <fubeitch@gmail.com>
2022-01-05 16:54:10 -0500

    Added abCfkZsPAk

--- API/src/test/java/com/lepine/transfers/http/WarehouseHttpTests.java
+++ API/src/test/java/com/lepine/transfers/http/WarehouseHttpTests.java
@@ -854,4 +854,20 @@ public class WarehouseHttpTests {
 
         verify(warehouseService, never()).update(VALID_UUID, given);
     }
+
+    @Test
+    @DisplayName("abCfkZsPAk: Given PUT on /warehouses/{uuid} with null zipcode as clerk, then return forbidden (403, error)")
+    @WithMockUser(username = "some-clerk", roles = "CLERK")
+    void update_AsClerk_WithNullZipcode() throws Exception {
+
+        // Arrange
+        final WarehouseUUIDLessDTO given = VALID_WAREHOUSE_UUID_LESS_DTO;
+        given.setZipCode(null);
+
+        // Act & Assert
+        updateWith(VALID_UUID, given, null)
+                .andExpect(status().isForbidden());
+
+        verify(warehouseService, never()).update(VALID_UUID, given);
+    }
 }
