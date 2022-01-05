3eafb16d6edfcfa00bb47a1691346ae28c54b03d
Fube <fubeitch@gmail.com>
2022-01-04 19:01:50 -0500

    Added WarehouseController::getAll impl

--- API/src/main/java/com/lepine/transfers/controllers/warehouse/WarehouseController.java
+++ API/src/main/java/com/lepine/transfers/controllers/warehouse/WarehouseController.java
@@ -5,10 +5,13 @@ import com.lepine.transfers.data.warehouse.WarehouseActiveLessUUIDLessDTO;
 import com.lepine.transfers.services.warehouse.WarehouseService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,10 +22,20 @@ public class WarehouseController {
     private final WarehouseService warehouseService;
 
     public Warehouse create(@Valid WarehouseActiveLessUUIDLessDTO warehouseActiveLessUUIDLessDTO) {
-        log.info("Attempting to create a new warehouse {}", warehouseActiveLessUUIDLessDTO);
+        log.info("Creating a new warehouse {}", warehouseActiveLessUUIDLessDTO);
         final Warehouse warehouse = warehouseService.create(warehouseActiveLessUUIDLessDTO);
         log.info("Warehouse created {}", warehouse);
 
         return warehouse;
+    }
+
+    public Page<Warehouse> getAll(
+            @Min(value = 1, message = "{pagination.page.min}") int pageNumber,
+            @Min(value = 1, message = "{pagination.size.min}") int pageSize) {
+        log.info("Getting all warehouses with pageNumber {} and pageSize {}", pageNumber, pageSize);
+        final Page<Warehouse> all = warehouseService.findAll(PageRequest.of(pageNumber, pageSize));
+        log.info("Warehouses found {}", all);
+
+        return all;
     }
 }
