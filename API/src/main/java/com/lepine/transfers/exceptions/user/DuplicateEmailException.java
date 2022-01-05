17f3dcc6c1fc0e069aeaa87e6adb9b2c4531428c
Fube <fubeitch@gmail.com>
2022-01-05 03:59:16 -0500

    Added DuplicateResourceException

--- API/src/main/java/com/lepine/transfers/exceptions/user/DuplicateEmailException.java
+++ API/src/main/java/com/lepine/transfers/exceptions/user/DuplicateEmailException.java
@@ -1,8 +1,10 @@
 package com.lepine.transfers.exceptions.user;
 
+import com.lepine.transfers.exceptions.DuplicateResourceException;
+
 import static java.lang.String.format;
 
-public class DuplicateEmailException extends RuntimeException {
+public class DuplicateEmailException extends DuplicateResourceException {
     public DuplicateEmailException(String email) {
         super(format("Email %s already in use", email));
     }
--- API/src/main/java/com/lepine/transfers/exceptions/warehouse/DuplicateZipCodeException.java
+++ API/src/main/java/com/lepine/transfers/exceptions/warehouse/DuplicateZipCodeException.java
@@ -1,8 +1,10 @@
 package com.lepine.transfers.exceptions.warehouse;
 
+import com.lepine.transfers.exceptions.DuplicateResourceException;
+
 import static java.lang.String.format;
 
-public class DuplicateZipCodeException extends RuntimeException {
+public class DuplicateZipCodeException extends DuplicateResourceException {
     public DuplicateZipCodeException(String zipCode) {
         super(format("Zipcode %s already in use", zipCode));
     }
