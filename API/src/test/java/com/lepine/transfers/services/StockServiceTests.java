56edc222088804ff464fd064e24b8d88eebb4299
jonathan ma <jonathan.marcotte.7@gmail.com>
2022-01-14 00:21:39 -0500

    Create service test skeleton

---
+++ API/src/test/java/com/lepine/transfers/services/StockServiceTests.java
@@ -0,0 +1,14 @@
+package com.lepine.transfers.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(classes = {})
+@ActiveProfiles({"test"})
+public class StockServiceTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
