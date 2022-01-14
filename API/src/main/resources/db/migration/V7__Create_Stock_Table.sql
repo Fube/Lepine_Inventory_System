8800e21dcd81684200dcb0a52d7c999d309ac091
jonathan ma <jonathan.marcotte.7@gmail.com>
2022-01-13 23:22:14 -0500

    DB Migration script fixed

--- API/src/main/resources/db/migration/V7__Create_Stock_Table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE lepine.warehouses (
-    uuid uuid PRIMARY KEY,
-    zip_code VARCHAR(255) NOT NULL UNIQUE,
-    city VARCHAR(255) NOT NULL,
-    province VARCHAR(64) NOT NULL,
-    active BOOLEAN NOT NULL DEFAULT true
-)
