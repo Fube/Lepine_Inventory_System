bd5f704398723224bc1beb7b6dc15ee4c02bf30b
Fube <fubeitch@gmail.com>
2022-01-15 11:57:29 -0500

    Got UXPSLjQjrx passing

--- API/src/test/java/com/lepine/transfers/data/StockDataTests.java
+++ API/src/test/java/com/lepine/transfers/data/StockDataTests.java
@@ -6,10 +6,7 @@ import com.lepine.transfers.data.stock.Stock;
 import com.lepine.transfers.data.stock.StockRepo;
 import com.lepine.transfers.data.warehouse.Warehouse;
 import com.lepine.transfers.data.warehouse.WarehouseRepo;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -23,11 +20,12 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @DataJpaTest
 @ActiveProfiles({"test"})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 public class StockDataTests {
 
-    private final static UUID
-        ITEM_UUID = UUID.randomUUID(),
-        WAREHOUSE_UUID = UUID.randomUUID();
+    private UUID
+            ITEM_UUID,
+            WAREHOUSE_UUID;
 
     @Autowired
     private ItemRepo itemRepo;
@@ -43,16 +41,17 @@ public class StockDataTests {
 
     @BeforeEach
     void setUp() {
-        itemRepo.save(new Item(ITEM_UUID, "Sku", "Description", "Name"));
-        warehouseRepo.save(new Warehouse(WAREHOUSE_UUID, "Zip", "City", "Province", true));
+        ITEM_UUID = itemRepo.save(new Item(ITEM_UUID, "Sku", "Description", "Name")).getUuid();
+        WAREHOUSE_UUID = warehouseRepo.save(
+                new Warehouse(WAREHOUSE_UUID, "Zip", "City", "Province", true)).getUuid();
         entityManager.flush();
     }
 
     @AfterEach
     void tearDown() {
+        stockRepo.deleteAll();
         itemRepo.deleteAll();
         warehouseRepo.deleteAll();
-        stockRepo.deleteAll();
         entityManager.flush();
     }
 
@@ -62,7 +61,6 @@ public class StockDataTests {
     @Test
     @DisplayName("UXPSLjQjrx: Given valid stock data when save, then save stock")
     void save_Valid() {
-
         // Arrange
         final int quantity = 10;
         final Stock stock = Stock.builder()
