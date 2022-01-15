fbe38dbf3bdb166c946abce0b971c2bf9ed01f93
Fube <fubeitch@gmail.com>
2022-01-15 11:32:16 -0500

    Added StockDataTests skeleton

---
+++ API/src/test/java/com/lepine/transfers/data/StockDataTests.java
@@ -0,0 +1,27 @@
+package com.lepine.transfers.data;
+
+import com.lepine.transfers.data.item.ItemRepo;
+import com.lepine.transfers.data.warehouse.WarehouseRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles({"test"})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class StockDataTests {
+
+    @Autowired
+    private ItemRepo itemRepo;
+
+    @Autowired
+    private WarehouseRepo warehouseRepo;
+
+    @Autowired
+    private StockRepo stockRepo;
+
+    @Test
+    void contextLoads(){}
+}
