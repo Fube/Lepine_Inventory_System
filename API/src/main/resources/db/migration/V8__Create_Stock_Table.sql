3fe651f620816529573d1dc3ba224f00349d027b
Fube <fubeitch@gmail.com>
2022-01-15 11:30:43 -0500

    Updated V8 migration

--- API/src/main/resources/db/migration/V8__Create_Stock_Table.sql
+++ API/src/main/resources/db/migration/V8__Create_Stock_Table.sql
@@ -2,5 +2,6 @@ CREATE TABLE lepine.stock(
     uuid uuid NOT NULL UNIQUE PRIMARY KEY,
     quantity integer NOT NULL,
     item_uuid uuid NOT NULL REFERENCES lepine.item(uuid) ON DELETE CASCADE,
-    warehouse_uuid uuid NOT NULL REFERENCES lepine.warehouse(uuid) ON DELETE CASCADE
+    warehouse_uuid uuid NOT NULL REFERENCES lepine.warehouse(uuid) ON DELETE CASCADE,
+    UNIQUE(item_uuid, warehouse_uuid)
 )
