df9c72a1880957957f56235bf82bae2b9deeeae8
Fube <fubeitch@gmail.com>
2022-01-16 06:25:41 -0500

    Added stock search stuff

--- API/src/main/java/com/lepine/transfers/services/search/SearchIndexProviders.java
+++ API/src/main/java/com/lepine/transfers/services/search/SearchIndexProviders.java
@@ -4,6 +4,7 @@ import com.algolia.search.SearchClient;
 import com.algolia.search.SearchIndex;
 import com.algolia.search.models.settings.IndexSettings;
 import com.lepine.transfers.data.item.ItemSearchDTO;
+import com.lepine.transfers.data.stock.StockSearchDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +21,14 @@ public class SearchIndexProviders {
     public SearchIndex<ItemSearchDTO> getItemSearchIndex() {
         final SearchIndex<ItemSearchDTO> index = searchClient.initIndex("items", ItemSearchDTO.class);
         index.setSettings(new IndexSettings().setSearchableAttributes(Arrays.asList("sku", "name", "description")));
+        return index;
+    }
+
+    @Bean
+    public SearchIndex<StockSearchDTO> getStockSearchIndex() {
+        final SearchIndex<StockSearchDTO> index = searchClient.initIndex("stocks", StockSearchDTO.class);
+        index.setSettings(new IndexSettings().setSearchableAttributes(
+                Arrays.asList("sku", "name", "description", "zipCode", "quantity")));
         return index;
     }
 }
