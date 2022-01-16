41f9a96d669e1a6d2a37bf0d002a3f2d4845bfe4
Fube <fubeitch@gmail.com>
2022-01-16 10:15:42 -0500

    Added JcXCHcgsfs

--- API/src/test/java/com/lepine/transfers/controllers/StockControllerTests.java
+++ API/src/test/java/com/lepine/transfers/controllers/StockControllerTests.java
@@ -1,15 +1,80 @@
 package com.lepine.transfers.controllers;
 
 import com.lepine.transfers.config.ValidationConfig;
+import com.lepine.transfers.data.item.Item;
+import com.lepine.transfers.data.stock.Stock;
+import com.lepine.transfers.data.stock.StockUuidLessItemLessWarehouseLess;
+import com.lepine.transfers.data.stock.StockUuidLessItemUuidWarehouseUuid;
+import com.lepine.transfers.data.warehouse.Warehouse;
 import com.lepine.transfers.services.stock.StockService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.BDDMockito.given;
+
 @SpringBootTest(classes = { ValidationConfig.class, StockController.class })
 @ActiveProfiles({"test"})
 public class StockControllerTests {
+
+    private final static String
+            ITEM_NOT_FOUND_ERROR_FORMAT = "Item with uuid %s not found",
+            WAREHOUSE_NOT_FOUND_ERROR_FORMAT = "Warehouse with uuid %s not found",
+            STOCK_NOT_FOUND_ERROR_FORMAT = "Stock with uuid %s not found";
+
+    private final static int VALID_QUANTITY = 10;
+    private final static UUID
+            VALID_ITEM_UUID = UUID.randomUUID(),
+            VALID_WAREHOUSE_UUID = UUID.randomUUID(),
+            VALID_STOCK_UUID = UUID.randomUUID();
+    private final static String
+            VALID_ITEM_NAME = "Item",
+            VALID_ITEM_DESCRIPTION = "Description",
+            VALID_ITEM_SKU = "SKU",
+            VALID_WAREHOUSE_ZIPCODE = "A1B2C3",
+            VALID_WAREHOUSE_CITY = "City",
+            VALID_WAREHOUSE_PROVINCE = "Province";
+
+    private final static Item VALID_ITEM = Item.builder()
+            .uuid(VALID_ITEM_UUID)
+            .name(VALID_ITEM_NAME)
+            .description(VALID_ITEM_DESCRIPTION)
+            .sku(VALID_ITEM_SKU)
+            .build();
+
+    private final static Warehouse VALID_WAREHOUSE = Warehouse.builder()
+            .uuid(VALID_WAREHOUSE_UUID)
+            .zipCode(VALID_WAREHOUSE_ZIPCODE)
+            .city(VALID_WAREHOUSE_CITY)
+            .province(VALID_WAREHOUSE_PROVINCE)
+            .build();
+
+    private final static Stock VALID_STOCK = Stock.builder()
+            .item(VALID_ITEM)
+            .warehouse(VALID_WAREHOUSE)
+            .quantity(VALID_QUANTITY)
+            .build();
+
+    private final static StockUuidLessItemLessWarehouseLess VALID_STOCK_UUID_LESS_ITEM_LESS_WAREHOUSE_LESS =
+            StockUuidLessItemLessWarehouseLess.builder()
+                    .quantity(VALID_QUANTITY)
+                    .build();
+
+    private final static StockUuidLessItemUuidWarehouseUuid VALID_STOCK_UUID_LESS_ITEM_UUID_WAREHOUSE_UUID =
+            StockUuidLessItemUuidWarehouseUuid.builder()
+                    .itemUuid(VALID_ITEM_UUID)
+                    .warehouseUuid(VALID_WAREHOUSE_UUID)
+                    .quantity(VALID_QUANTITY)
+                    .build();
+
+    @Autowired
+    private StockController stockController;
 
     @MockBean
     private StockService stockService;
@@ -17,5 +82,17 @@ public class StockControllerTests {
     @Test
     void contextLoads(){}
 
+    @Test
+    @DisplayName("JcXCHcgsfs: Given valid dto when create, then return stock")
+    void create_Valid() {
+        // Arrange
+        given(stockService.create(VALID_STOCK_UUID_LESS_ITEM_UUID_WAREHOUSE_UUID))
+                .willReturn(VALID_STOCK);
 
+        // Act
+        Stock result = stockController.create(VALID_STOCK_UUID_LESS_ITEM_UUID_WAREHOUSE_UUID);
+
+        // Assert
+        assertThat(result).isEqualTo(VALID_STOCK);
+    }
 }
