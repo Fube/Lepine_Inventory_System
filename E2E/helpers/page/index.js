f46534a4cc2faf65e54e789c1b8e95da8b9108e6
Fube <fubeitch@gmail.com>
2022-01-16 15:12:20 -0500

    Added waitForTitle helper

--- E2E/helpers/page/index.js
+++ E2E/helpers/page/index.js
@@ -12,6 +12,19 @@ async function clearThenType(page, input, newValue) {
     await input.type(newValue);
 }
 
+/**
+ *
+ * @param {import("playwright-core").Page} page
+ * @param {import("playwright-core").Locator} input
+ * @param {*} newValue
+ */
+async function waitForTitle(page, title) {
+    return page.waitForFunction(
+        () => document?.querySelector("title")?.text === title
+    );
+}
+
 module.exports = {
     clearThenType,
+    waitForTitle,
 };
