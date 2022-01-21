a6a7ebb35785a5c140eb81012a02e137d90eaa52
Fube <fubeitch@gmail.com>
2022-01-20 23:30:44 -0500

    Added Jackson jsr310 dep and module

--- API/src/main/java/com/lepine/transfers/config/JacksonConfig.java
+++ API/src/main/java/com/lepine/transfers/config/JacksonConfig.java
@@ -1,6 +1,7 @@
 package com.lepine.transfers.config;
 
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,9 @@ public class JacksonConfig {
 
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer getJsonCustomizer() {
-        return builder -> builder.modules(new Hibernate5Module());
+        return builder -> builder.modules(
+                new Hibernate5Module(),
+                new JavaTimeModule()
+        );
     }
 }
