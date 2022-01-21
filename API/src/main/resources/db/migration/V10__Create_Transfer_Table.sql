bb5a62b0f1030f8f711afcc4317a6b7f8f87da30
Fube <fubeitch@gmail.com>
2022-01-21 16:10:09 -0500

    Fixed 'to' column being in transfer instead of shipment

--- API/src/main/resources/db/migration/V10__Create_Transfer_Table.sql
+++ API/src/main/resources/db/migration/V10__Create_Transfer_Table.sql
@@ -3,6 +3,5 @@ CREATE TABLE lepine.transfers (
       quantity integer NOT NULL,
       stock_uuid uuid NOT NULL REFERENCES lepine.stocks(uuid) ON DELETE CASCADE,
       shipment_uuid uuid NOT NULL REFERENCES lepine.shipments(uuid) ON DELETE CASCADE,
-      to uuid NOT NULL REFERENCES lepine.warehouses(uuid),
       PRIMARY KEY (uuid)
 )
