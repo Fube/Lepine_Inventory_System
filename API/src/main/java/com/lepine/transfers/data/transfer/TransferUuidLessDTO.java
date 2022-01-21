bb17644c17270aa58a0715a11155c2784ffb5240
Fube <fubeitch@gmail.com>
2022-01-21 18:56:35 -0500

    Added min annotation to TransferUuiDLessDTO::quantity

--- API/src/main/java/com/lepine/transfers/data/transfer/TransferUuidLessDTO.java
+++ API/src/main/java/com/lepine/transfers/data/transfer/TransferUuidLessDTO.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
 import java.util.UUID;
 
 @Data
@@ -14,5 +15,7 @@ import java.util.UUID;
 public class TransferUuidLessDTO {
 
     private UUID stockUuid;
+
+    @Min(value = 1, message = "{transfer.quantity.min}")
     private int quantity;
 }
