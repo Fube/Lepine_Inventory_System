058a6ac4efed92a7d5cbd3e1be1f8f654a4312da
Fube <fubeitch@gmail.com>
2022-01-22 01:07:17 -0500

    Added AeXLlOffFQ

--- API/src/test/java/com/lepine/transfers/unit/controllers/ShipmentControllerTests.java
+++ API/src/test/java/com/lepine/transfers/unit/controllers/ShipmentControllerTests.java
@@ -7,19 +7,27 @@ import com.lepine.transfers.data.auth.Role;
 import com.lepine.transfers.data.shipment.Shipment;
 import com.lepine.transfers.data.user.User;
 import com.lepine.transfers.services.shipment.ShipmentService;
+import com.lepine.transfers.utils.ConstraintViolationExceptionUtils;
+import com.lepine.transfers.utils.MessageSourceUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ActiveProfiles;
 
+import javax.validation.ConstraintViolationException;
+import java.util.Set;
 import java.util.UUID;
 
+import static com.lepine.transfers.utils.MessageSourceUtils.wrapperFor;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowableOfType;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
@@ -47,11 +55,26 @@ public class ShipmentControllerTests {
                 .name(VALID_MANAGER_ROLE_NAME)
                 .build();
 
+    private String
+            ERROR_MESSAGE_PAGINATION_PAGE_MIN,
+            ERROR_MESSAGE_PAGINATION_SIZE_MIN;
+
     @Autowired
     private ShipmentController shipmentController;
 
+    @Autowired
+    private ReloadableResourceBundleMessageSource messageSource;
+
+
     @MockBean
     private ShipmentService shipmentService;
+
+    @BeforeEach
+    void setUp() {
+        final MessageSourceUtils.ForLocaleWrapper w = wrapperFor(messageSource);
+        ERROR_MESSAGE_PAGINATION_PAGE_MIN = w.getMessage("pagination.page.min");
+        ERROR_MESSAGE_PAGINATION_SIZE_MIN = w.getMessage("pagination.size.min");
+    }
 
     @Test
     void contextLoads() {}
@@ -123,5 +146,32 @@ public class ShipmentControllerTests {
                                 && pr.getPageSize() == size
                                 && pr.getSort().equals(Sort.by("expectedDate").descending())));
         verify(shipmentService, never()).findAllByUserUuid(any(), any());
+    }
+
+    @Test
+    @DisplayName("AeXLlOffFQ: Given page < 1 when get, then throw ConstraintViolationException")
+    void invalid_PageLessThan1() {
+
+        // Arrange
+        final int
+                page = 0, // One-indexed
+                size = 10;
+        final User givenUser = User.builder()
+                .uuid(VALID_USER_UUID)
+                .email(VALID_USER_EMAIL)
+                .password(VALID_USER_PASSWORD)
+                .role(VALID_MANAGER_ROLE)
+                .build();
+
+        // Act
+        final ConstraintViolationException constraintViolationException = catchThrowableOfType(
+                () -> shipmentController.findAll(givenUser, page, size), ConstraintViolationException.class);
+
+        // Assert
+        final Set<String> collect = ConstraintViolationExceptionUtils.extractMessages(constraintViolationException);
+        assertThat(collect).containsExactly("Page must be greater than or equal to 1");
+
+        verify(shipmentService, never()).findAllByUserUuid(any(), any());
+        verify(shipmentService, never()).findAll(any());
     }
 }
