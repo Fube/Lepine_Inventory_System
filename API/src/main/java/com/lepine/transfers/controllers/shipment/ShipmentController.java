358629e73674dca05c90b5a035116e15b0981e79
Fube <fubeitch@gmail.com>
2022-01-22 01:08:46 -0500

    Added validation annotation for ShipmentController

--- API/src/main/java/com/lepine/transfers/controllers/shipment/ShipmentController.java
+++ API/src/main/java/com/lepine/transfers/controllers/shipment/ShipmentController.java
@@ -10,17 +10,22 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.Min;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+@Validated
 public class ShipmentController {
 
     private final ShipmentService shipmentService;
     private final ShipmentMapper shipmentMapper;
 
-    public Page<Shipment> findAll(@AuthenticationPrincipal User user, final int page, final int size) {
+    public Page<Shipment> findAll(@AuthenticationPrincipal User user,
+                                  @Min(value = 1, message = "{pagination.page.min}") final int page, final int size) {
         final PageRequest pageRequest = PageRequest.of(page - 1, size, Sort.by("expectedDate").descending());
 
         log.info("Fetching all shipments for user {} with page request {}", user.getUsername(), pageRequest);
