43f812e62a0c7f395d93524a801316e99f382667
Fube <fubeitch@gmail.com>
2022-01-21 23:02:59 -0500

    Got qKQnksVZXM passing

--- API/src/main/java/com/lepine/transfers/data/shipment/ShipmentRepo.java
+++ API/src/main/java/com/lepine/transfers/data/shipment/ShipmentRepo.java
@@ -2,9 +2,11 @@ package com.lepine.transfers.data.shipment;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -19,4 +21,8 @@ public interface ShipmentRepo extends JpaRepository<Shipment, UUID> {
     @EntityGraph(attributePaths = {"transfers.stock", "transfers.stock.item", "transfers.stock.warehouse"})
     @Query("select s from Shipment s")
     Page<Shipment> findAll(PageRequest pageRequest);
+
+    @EntityGraph(attributePaths = {"transfers.stock", "transfers.stock.item", "transfers.stock.warehouse"})
+    @Query("select s from Shipment s where s.createdBy = :createdBy")
+    Page<Shipment> findAllByCreatedBy(@Param("createdBy") UUID createdBy, Pageable pageable);
 }
