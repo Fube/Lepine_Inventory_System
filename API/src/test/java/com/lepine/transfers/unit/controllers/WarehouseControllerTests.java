d23dc9bced785cb5bd0b9beee710a9da0ce21d4e
Fube <fubeitch@gmail.com>
2022-01-25 22:53:20 -0500

    Fixed broken tests

--- API/src/test/java/com/lepine/transfers/unit/controllers/WarehouseControllerTests.java
+++ API/src/test/java/com/lepine/transfers/unit/controllers/WarehouseControllerTests.java
@@ -278,7 +278,8 @@ public class WarehouseControllerTests {
 
         // Act
         final ConstraintViolationException constraintViolationException =
-                assertThrows(ConstraintViolationException.class, () -> warehouseController.getAll(pageNumber, pageSize));
+                assertThrows(ConstraintViolationException.class,
+                        () -> warehouseController.getAll(pageNumber, pageSize, Optional.empty()));
 
         // Assert
         final Set<String> collect = ConstraintViolationExceptionUtils.extractMessages(constraintViolationException);
@@ -296,7 +297,8 @@ public class WarehouseControllerTests {
 
         // Act
         final ConstraintViolationException constraintViolationException =
-                assertThrows(ConstraintViolationException.class, () -> warehouseController.getAll(pageNumber, pageSize));
+                assertThrows(ConstraintViolationException.class,
+                        () -> warehouseController.getAll(pageNumber, pageSize, Optional.empty()));
 
         // Assert
         final Set<String> collect = ConstraintViolationExceptionUtils.extractMessages(constraintViolationException);
@@ -314,7 +316,8 @@ public class WarehouseControllerTests {
 
         // Act
         final ConstraintViolationException constraintViolationException =
-                assertThrows(ConstraintViolationException.class, () -> warehouseController.getAll(pageNumber, pageSize));
+                assertThrows(ConstraintViolationException.class,
+                        () -> warehouseController.getAll(pageNumber, pageSize, Optional.empty()));
 
         // Assert
         final Set<String> collect = ConstraintViolationExceptionUtils.extractMessages(constraintViolationException);
@@ -336,7 +339,7 @@ public class WarehouseControllerTests {
         when(warehouseService.findAll(expectedPageRequest)).thenReturn(expected);
 
         // Act
-        final Page<Warehouse> actual = warehouseController.getAll(pageNumber, pageSize);
+        final Page<Warehouse> actual = warehouseController.getAll(pageNumber, pageSize, Optional.empty());
 
         // Assert
         assertThat(actual.getTotalPages()).isEqualTo(expected.getTotalPages());
