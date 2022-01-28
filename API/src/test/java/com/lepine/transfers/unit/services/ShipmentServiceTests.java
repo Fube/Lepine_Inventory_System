b655d197d17689fdff73dfbfb3e00287e2377cc3
Fube <fubeitch@gmail.com>
2022-01-28 04:07:14 -0500

    Added ShipmentNotPendingException

--- API/src/test/java/com/lepine/transfers/unit/services/ShipmentServiceTests.java
+++ API/src/test/java/com/lepine/transfers/unit/services/ShipmentServiceTests.java
@@ -13,6 +13,7 @@ import com.lepine.transfers.data.stock.Stock;
 import com.lepine.transfers.data.transfer.TransferUuidLessDTO;
 import com.lepine.transfers.data.warehouse.Warehouse;
 import com.lepine.transfers.exceptions.shipment.ShipmentNotFoundException;
+import com.lepine.transfers.exceptions.shipment.ShipmentNotPendingException;
 import com.lepine.transfers.exceptions.transfer.SameWarehouseException;
 import com.lepine.transfers.exceptions.warehouse.WarehouseNotFoundException;
 import com.lepine.transfers.services.shipment.ShipmentService;
