7bf3d12e9c012a328fa10a1882d0673db3aca9c3
Fube <fubeitch@gmail.com>
2022-01-27 21:33:54 -0500

    Fixed V9 not escaping to

--- API/src/main/resources/db/migration/V9__Create_Shipment_Table.sql
+++ API/src/main/resources/db/migration/V9__Create_Shipment_Table.sql
@@ -4,6 +4,6 @@ CREATE TABLE lepine.shipments (
       expected_date timestamp with time zone NOT NULL,
       order_number varchar(255) NOT NULL,
       created_by uuid NOT NULL REFERENCES lepine.users(uuid),
-      to uuid NOT NULL REFERENCES lepine.warehouses(uuid),
+      "to" uuid NOT NULL REFERENCES lepine.warehouses(uuid),
       PRIMARY KEY (uuid)
 )
