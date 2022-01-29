eb6104328189cbb406ace1f733f2f06b00f3b521
Fube <fubeitch@gmail.com>
2022-01-29 00:58:08 -0500

    Added SengridConfig

---
+++ API/src/main/java/com/lepine/transfers/config/SendGridConfig.java
@@ -0,0 +1,29 @@
+package com.lepine.transfers.config;
+
+import com.lepine.transfers.services.mailer.MailerService;
+import com.sendgrid.SendGrid;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.NotNull;
+
+@Component
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix = "sendgrid")
+@Data
+public class SendGridConfig {
+
+    @NotNull
+    private String apiKey;
+
+    @NotNull
+    private String from;
+
+    @Bean
+    public SendGrid getSendGrid() {
+        return new SendGrid(apiKey);
+    }
+}
