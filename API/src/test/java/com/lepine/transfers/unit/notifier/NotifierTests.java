f66830f83ee9d6a863b5f211ef2edc161eefd7f0
Fube <fubeitch@gmail.com>
2022-01-29 02:32:27 -0500

    Added BMWhlJynWR

--- API/src/test/java/com/lepine/transfers/unit/notifier/NotifierTests.java
+++ API/src/test/java/com/lepine/transfers/unit/notifier/NotifierTests.java
@@ -1,22 +1,50 @@
 package com.lepine.transfers.unit.notifier;
 
+import com.lepine.transfers.data.shipment.Shipment;
+import com.lepine.transfers.data.shipment.ShipmentStatus;
+import com.lepine.transfers.data.user.User;
+import com.lepine.transfers.events.shipment.ShipmentUpdateEvent;
 import com.lepine.transfers.notification.Notifier;
 import com.lepine.transfers.services.mailer.MailerService;
 import com.lepine.transfers.services.user.UserService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 @SpringBootTest(classes= {
-        Notifier.class
+        Notifier.class,
 })
 @ActiveProfiles({"test"})
 public class NotifierTests {
 
+    private final static UUID
+        VALID_USER_UUID =  UUID.randomUUID();
+
+    private final static String
+        VALID_USER_EMAIL = "valid@email.com";
+
+    private final static User
+        VALID_USER = User.builder()
+            .uuid(VALID_USER_UUID)
+            .email(VALID_USER_EMAIL)
+            .build();
+
     @Autowired
     private Notifier notifier;
+
+    @Autowired
+    private ApplicationEventPublisher applicationEventPublisher;
 
     @MockBean
     private UserService userService;
@@ -26,4 +54,38 @@ public class NotifierTests {
 
     @Test
     void contextLoads(){}
+
+    @Test
+    @DisplayName("BMWhlJynWR: Given shipment update, trigger onShipmentUpdate")
+    void valid_TriggerShipmentUpdate() {
+
+        // Arrange
+        final Shipment expectedOldShipment = Shipment.builder()
+                .uuid(UUID.randomUUID())
+                .createdBy(VALID_USER_UUID)
+                .status(ShipmentStatus.PENDING)
+                .build();
+
+        final Shipment expectedNewShipment = expectedOldShipment.toBuilder()
+                .status(ShipmentStatus.ACCEPTED)
+                .build();
+
+        final ShipmentUpdateEvent expectedUpdateShipmentEvent =
+                new ShipmentUpdateEvent(this, expectedOldShipment, expectedNewShipment);
+
+        final String expectedSubject = "Shipment status update";
+        final String expectedBody = "Your shipment has been accepted";
+
+        given(userService.findByUuid(VALID_USER_UUID)).willReturn(Optional.of(VALID_USER));
+        given(mailerService.sendHTML(VALID_USER_EMAIL, expectedSubject, expectedBody))
+                .willReturn(true);
+
+        // Act
+        applicationEventPublisher.publishEvent(expectedUpdateShipmentEvent);
+
+        // Assert
+        verify(userService, times(1)).findByUuid(VALID_USER_UUID);
+        verify(mailerService, times(1))
+                .sendHTML(VALID_USER_EMAIL, expectedSubject, expectedBody);
+    }
 }
