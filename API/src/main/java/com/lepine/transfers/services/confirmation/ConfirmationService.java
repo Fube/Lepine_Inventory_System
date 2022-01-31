a8763a7460872e8d16ac9b032cbb6696e4c90ac4
Fube <fubeitch@gmail.com>
2022-01-31 01:26:57 -0500

    Fixed ConfirmationService::confirm not interpolating message

--- API/src/main/java/com/lepine/transfers/services/confirmation/ConfirmationService.java
+++ API/src/main/java/com/lepine/transfers/services/confirmation/ConfirmationService.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 
 public interface ConfirmationService {
     Confirmation confirm(
-            @NotNull(message = "transfer.uuid.not_null") final UUID transferUuid,
+            @NotNull(message = "{transfer.uuid.not_null}") final UUID transferUuid,
             final int quantity
     );
 }
