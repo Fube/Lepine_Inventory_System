86b54280d86d1f60576c0d69f4544ace51f68fcc
Fube <fubeitch@gmail.com>
2022-01-31 03:49:56 -0500

    Added Min to ConfirmationUuidLessDTO

--- API/src/main/java/com/lepine/transfers/data/confirmation/ConfirmationUuidLessDTO.java
+++ API/src/main/java/com/lepine/transfers/data/confirmation/ConfirmationUuidLessDTO.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
 import java.util.UUID;
 
 @Data
@@ -14,5 +15,7 @@ import java.util.UUID;
 public class ConfirmationUuidLessDTO {
 
     private UUID transferUuid;
+
+    @Min(value = 1, message = "{transfer.quantity.min}")
     private int quantity;
 }
