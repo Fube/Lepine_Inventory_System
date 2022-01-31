64d9e140485f6ed74deb9378f986af1ca2e2325e
Fube <fubeitch@gmail.com>
2022-01-31 03:47:47 -0500

    Got ZSdEdDwCnP passing

--- API/src/main/java/com/lepine/transfers/controllers/confirmation/ConfirmationController.java
+++ API/src/main/java/com/lepine/transfers/controllers/confirmation/ConfirmationController.java
@@ -1,10 +1,14 @@
 package com.lepine.transfers.controllers.confirmation;
 
+import com.lepine.transfers.data.confirmation.Confirmation;
+import com.lepine.transfers.data.confirmation.ConfirmationUuidLessDTO;
 import com.lepine.transfers.services.confirmation.ConfirmationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,4 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ConfirmationController {
 
     private final ConfirmationService confirmationService;
+
+    public Confirmation create(ConfirmationUuidLessDTO confirmationUuidLessDto) {
+
+        final UUID transferUuid = confirmationUuidLessDto.getTransferUuid();
+        final int quantity = confirmationUuidLessDto.getQuantity();
+        log.info("Confirming {} for {}", transferUuid, quantity);
+
+        return confirmationService.confirm(transferUuid, quantity);
+    }
 }
