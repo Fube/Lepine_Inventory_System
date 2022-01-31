66075070739a016ee04ba7703afa5c04f71258ee
Fube <fubeitch@gmail.com>
2022-01-31 01:25:27 -0500

    Added NotNull to ConfirmationService::confirm

--- API/src/main/java/com/lepine/transfers/services/confirmation/ConfirmationService.java
+++ API/src/main/java/com/lepine/transfers/services/confirmation/ConfirmationService.java
@@ -2,8 +2,12 @@ package com.lepine.transfers.services.confirmation;
 
 import com.lepine.transfers.data.confirmation.Confirmation;
 
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 public interface ConfirmationService {
-    Confirmation confirm(final UUID transferUuid, final int quantity);
+    Confirmation confirm(
+            @NotNull(message = "transfer.uuid.not_null") final UUID transferUuid,
+            final int quantity
+    );
 }
