40dbc723d12abdcdc9ea1c9ecde39c8056450f42
Fube <fubeitch@gmail.com>
2022-02-23 11:43:55 -0500

    i18n'd stats/index

--- UI/src/pages/stats/index.jsx
+++ UI/src/pages/stats/index.jsx
@@ -1,3 +1,5 @@
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Head from "next/head";
 import Link from "next/link";
 import WithClientSideAuth from "../../components/WithClientSideAuth";
@@ -11,9 +13,12 @@ import WithClientSideAuth from "../../components/WithClientSideAuth";
  * }}
  */
 function ShowStats({}) {
+    const { t: tc } = useTranslation("common");
+    const { t: ts } = useTranslation("stats");
+
     const header = (
         <Head>
-            <title>Stats</title>
+            <title>{ts("index.title")}</title>
         </Head>
     );
 
@@ -25,18 +30,18 @@ function ShowStats({}) {
                 <div className="md:w-3/4 lg:w-1/2 w-full">
                     <div className="md:flex justify-around my-4">
                         <h1 className="text-4xl md:mb-0 mb-4 text-center">
-                            <p className="mb-2">Stats</p>
+                            <p className="mb-2">{ts("index.title")}</p>
                         </h1>
                     </div>
                     <div className="flex flex-col gap-2">
                         <Link href="/stats/shipments/confirmed" passHref>
                             <button className="btn text-lg">
-                                Fully Confirmed Shipments
+                                {ts("shipments.fully_confirmed")}
                             </button>
                         </Link>
                         <Link href="/stats/shipments/sold" passHref>
                             <button className="btn text-lg">
-                                Best Selling Items By Week
+                                {ts("items.best_selling.by_week")}
                             </button>
                         </Link>
                     </div>
@@ -51,6 +56,17 @@ function ShowStats({}) {
  * @param {import("next").GetServerSidePropsContext} context
  * @returns
  */
-// export async function getServerSideProps(context) {}
+export async function getServerSideProps(context) {
+    const i18n = await serverSideTranslations(context.locale, [
+        "common",
+        "stats",
+    ]);
+
+    return {
+        props: {
+            ...i18n,
+        },
+    };
+}
 
 export default WithClientSideAuth(ShowStats);
