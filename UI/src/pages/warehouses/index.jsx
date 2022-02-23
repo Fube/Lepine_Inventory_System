c613a30c841d5aaceb7f5a355a909283f866eb98
Fube <fubeitch@gmail.com>
2022-02-23 14:59:20 -0500

    i18n'd warehouses/index

--- UI/src/pages/warehouses/index.jsx
+++ UI/src/pages/warehouses/index.jsx
@@ -1,9 +1,10 @@
+import { Icon } from "@iconify/react";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { Icon } from "@iconify/react";
 import Paginate from "../../components/Pagination";
-import Nav from "../../components/Nav";
 import { axiosBackendAuth } from "../../config/axios";
 import useAuth from "../../hooks/useAuth";
 import thou from "../../utils/thou";
@@ -11,21 +12,24 @@ import thou from "../../utils/thou";
  * @param {{ warehouses: import('@lepine/ui-types').Warehouse[] } & import("@lepine/ui-types").Pagination} param0
  */
 export default function ShowWarehouses({ warehouses, totalPages, pageNumber }) {
+    const { t: tc } = useTranslation("common");
+    const { t: tw } = useTranslation("warehouses");
+
     const router = useRouter();
     const { role } = useAuth();
 
     const header = (
         <Head>
-            <title>Warehouses</title>
+            <title>{tw("index.title")}</title>
         </Head>
     );
 
     const head = (
         <tr>
-            <th>Zip Code</th>
-            <th>City</th>
+            <th>{tc("zipcode")}</th>
+            <th>{tc("city")}</th>
             <th className="md:rounded-r-none rounded-r-lg flex justify-between md:table-cell">
-                <div className="self-center">Province</div>
+                <div className="self-center">{tc("province")}</div>
                 {role === "MANAGER" ? (
                     <button className="md:hidden">
                         <Link href="/warehouses/new" passHref>
@@ -39,7 +43,7 @@ export default function ShowWarehouses({ warehouses, totalPages, pageNumber }) {
             <th className="hidden justify-between md:flex">
                 {thou(
                     <>
-                        <div className="self-center">Active</div>
+                        <div className="self-center">{tc("active")}</div>
                         <button>
                             <Link href="/warehouses/new" passHref>
                                 <Icon icon="si-glyph:button-plus" width="32" />
@@ -47,16 +51,14 @@ export default function ShowWarehouses({ warehouses, totalPages, pageNumber }) {
                         </button>
                     </>
                 )
-                    .or("Active")
+                    .or(tc("active"))
                     .if(role === "MANAGER")}
             </th>
         </tr>
     );
 
     const fallback = (
-        <h2 className="text-2xl text-center text-yellow-400">
-            No warehouses to show 😢
-        </h2>
+        <h2 className="text-2xl text-center text-yellow-400">{tw("none")}</h2>
     );
 
     if (warehouses.length <= 0) {
@@ -70,7 +72,7 @@ export default function ShowWarehouses({ warehouses, totalPages, pageNumber }) {
                         {role === "MANAGER" && (
                             <Link href="/warehouses/new" passHref>
                                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-12">
-                                    Add One Now!
+                                    {tc("add_one_now")}
                                 </button>
                             </Link>
                         )}
@@ -87,7 +89,9 @@ export default function ShowWarehouses({ warehouses, totalPages, pageNumber }) {
             <div className="overflow-x-auto justify-center flex">
                 <div className="md:w-3/4 lg:w-1/2 w-full">
                     <div className="md:flex justify-around my-4">
-                        <h1 className="text-4xl md:mb-0 mb-4">Warehouses</h1>
+                        <h1 className="text-4xl md:mb-0 mb-4">
+                            {tw("index.title")}
+                        </h1>
                     </div>
                     <table className="table table-zebra w-full sm:table-fixed">
                         <thead>{head}</thead>
@@ -117,6 +121,8 @@ export default function ShowWarehouses({ warehouses, totalPages, pageNumber }) {
  * @param {Warehouse} param0
  */
 function WarehouseTableRow({ uuid, zipCode, city, province, active }) {
+    const { t: tc } = useTranslation("common");
+
     return (
         <Link key={uuid} href={`/warehouses/${uuid}`} passHref>
             <tr className="hover">
@@ -126,7 +132,7 @@ function WarehouseTableRow({ uuid, zipCode, city, province, active }) {
                     {province}
                 </td>
                 <td className="td-wrap hidden md:table-cell">
-                    {active ? "Yes" : "No"}
+                    {tc(active ? "yes" : "no")}
                 </td>
             </tr>
         </Link>
@@ -144,12 +150,18 @@ export async function getServerSideProps(context) {
         headers: { cookie: context?.req?.headers?.cookie ?? "" },
     });
 
+    const i18n = await serverSideTranslations(context.locale, [
+        "common",
+        "warehouses",
+    ]);
+
     return res
         .refine(({ content: warehouses, totalPages, number: pageNumber }) => ({
             props: {
                 warehouses,
                 totalPages,
                 pageNumber,
+                ...i18n,
             },
         }))
         .get();
